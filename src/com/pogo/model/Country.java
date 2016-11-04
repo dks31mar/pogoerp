dcafd971155895c7a900858fc5c40b6475dd526d
dks31mar@gmail.com <dks31mar@gmail.com>
2016-11-04 18:24:56 +0530

    dks

---
+++ src/com/pogo/model/Country.java
@@ -0,0 +1,39 @@
+package com.pogo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="country")
+public class Country implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "countryId")
+	private Integer countryId;
+	
+	public Integer getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(Integer countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Column(name = "country")
+	private String country;
+
+}
