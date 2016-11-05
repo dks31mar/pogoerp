cd92c76d7334bc09ad44cec136172e51373c9ca8
Deepak kumar <dks31mar@gmail.com>
2016-11-05 17:42:21 +0530

    dks

--- src/com/pogo/controller/MasterController.java
+++ src/com/pogo/controller/MasterController.java
@@ -28,13 +28,9 @@ import org.springframework.web.servlet.ModelAndView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.google.gson.Gson;
-
 import com.pogo.bean.CompanyInfoBean;
 
 import com.pogo.bean.DesignationBean;
-import com.pogo.bean.PoRefEntryItemDetailCopyBean;
-
 import com.pogo.bean.CountryBean;
 import com.pogo.bean.CurrencyBean;
 
@@ -54,14 +50,7 @@ import com.pogo.dao.UserEmployeeDao;
 import com.pogo.model.CompanyInfo;
 
 import com.pogo.model.Designation;
-import com.pogo.model.PoRefEntryItemDetailCopy;
-
-
 import com.pogo.model.Country;
-import com.pogo.model.Currency;
-
-
-
 import com.pogo.model.CustomerLevels;
 
 
@@ -93,8 +82,7 @@ public class MasterController
 	UserEmployeeDao empDao;
 
 	
-	@Autowired
-	private MasterProductService masterProductService;
+	
 	
 	
 	@Autowired
@@ -301,299 +289,25 @@ String getDesignation()
 		return comp;
 	}
 
-	/*
-	 * Master product
-	 * 
-	 * currency by dks
-	 * */
 	
 	
-	@RequestMapping(value="/currency",method = RequestMethod.GET)
-	public ModelAndView getCurrency(@ModelAttribute("currencyBean") CurrencyBean currencyBean,
+	@RequestMapping(value="/appregis",method = RequestMethod.GET)
+	public ModelAndView MobileAppRegis(@ModelAttribute("producthead") ProductHeadBean productHeadBean,
 			HttpServletRequest request){
-		List<CurrencyBean> list=new ArrayList<CurrencyBean>();
-		list=masterProductService.getCurrencyDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("currencylist", list);
 		
-			
-		
-		return new ModelAndView("currency",model);
+		return new ModelAndView("appregis");
 }
-
 	
-
-	
-	@RequestMapping(value="savecurrency",method=RequestMethod.POST)
-	@ResponseBody
-	public String addCurrency(@RequestBody String json,Model model) throws IOException{
-	System.out.println(json);
-		ObjectMapper mapper=new ObjectMapper();
-		CurrencyBean poref=mapper.readValue(json, CurrencyBean.class);
-		CurrencyBean poref1=new CurrencyBean();
-		poref1.setCurrencyname(poref.getCurrencyname());
-		poref1.setCurrencysymbol(poref.getCurrencysymbol());
-		poref1.setCurrencytype(poref.getCurrencytype());
-		
-		//poref.setPorefentryitemdetailid(null);
-		
-		
-		
-		masterProductService.addCurrency(poref1);
-		//model.addAttribute("prolist",  prepareListofBean(prinicipalposervice.proList()));
-	return toJson(poref1);
-	}
-
-	private String toJson(CurrencyBean poRefEntry) {
-		ObjectMapper mapper = new ObjectMapper();
-	    try {
-	        String value = mapper.writeValueAsString(poRefEntry);
-	        // return "["+value+"]";
-	        return value;
-	    } catch (JsonProcessingException e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
-	
-
-	@RequestMapping(value = "deletecurrency", method = RequestMethod.GET)
-	public ModelAndView deleteuserCurrencyData(@RequestParam("id") int id) {
-		masterProductService.deleteCurrency(id);
-		List<CurrencyBean> list=new ArrayList<CurrencyBean>();
-		list=masterProductService.getCurrencyDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("currencylist", list);
-		
-			
-		
-		return new ModelAndView("currency",model);
-	}
-	
-	
-	@RequestMapping(value = "getcurrency", method = RequestMethod.POST)
-	public void getCurrency(@RequestParam("id") String id,HttpServletResponse res )throws ParseException  {
-		String curList=masterProductService.getCurrency(id);
-		System.out.println(curList);
-		try {
-			PrintWriter writter=res.getWriter();
-			writter.print(curList);
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-	
-	}
-	
-	
-	@RequestMapping(value="editcurrency",method=RequestMethod.POST)
-	@ResponseBody
-	public String editCurrency(@RequestBody String json,Model model) throws IOException{
-	System.out.println("Deeoak              *****************************************************");
-		ObjectMapper mapper=new ObjectMapper();
-		CurrencyBean poref=mapper.readValue(json, CurrencyBean.class);
-		CurrencyBean poref1=new CurrencyBean();
-		poref1.setCurrencyid(poref.getCurrencyid());
-		poref1.setCurrencyname(poref.getCurrencyname());
-		poref1.setCurrencysymbol(poref.getCurrencysymbol());
-		poref1.setCurrencytype(poref.getCurrencytype());
-		
-		//poref.setPorefentryitemdetailid(null);
-		
-		
-		
-		masterProductService.editCurrency(poref1);
-		//model.addAttribute("prolist",  prepareListofBean(prinicipalposervice.proList()));
-	return toJson1(poref1);
-	}
-
-	private String toJson1(CurrencyBean poRefEntry) {
-		ObjectMapper mapper = new ObjectMapper();
-	    try {
-	        String value = mapper.writeValueAsString(poRefEntry);
-	        // return "["+value+"]";
-	        return value;
-	    } catch (JsonProcessingException e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
-	
-	@RequestMapping(value="/unit",method = RequestMethod.GET)
-	public ModelAndView getUnits(@ModelAttribute("unitBean") UnitBean currencyBean,
+	@RequestMapping(value="/promaster",method = RequestMethod.GET)
+	public ModelAndView productMaster(@ModelAttribute("producthead") ProductHeadBean productHeadBean,
 			HttpServletRequest request){
-		List<UnitBean> list=new ArrayList<UnitBean>();
-		list=masterProductService.getUnitsDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("unitlist", list);
-		return new ModelAndView("getunit",model);
+		
+		
+		return new ModelAndView("promaster");
 }
 	
 	
-	@RequestMapping(value="addunit",method=RequestMethod.POST)
-	@ResponseBody
-	public void addUnit(@RequestBody String json,Model model) throws IOException{
-	System.out.println("Deeoak              ***************************************************** \n"+json);
-		ObjectMapper mapper=new ObjectMapper();
-		UnitBean poref=mapper.readValue(json, UnitBean.class);
-		UnitBean poref1=new UnitBean();
-		//poref1.setCurrencyid(poref.getCurrencyid());
-		poref1.setUnittype(poref.getUnittype());
-		
-		//poref.setPorefentryitemdetailid(null);
-		
-		
-		
-		masterProductService.addUnit(poref1);
-		//model.addAttribute("prolist",  prepareListofBean(prinicipalposervice.proList()));
-	//return toJson1(poref1);
-	}
 	
-	@RequestMapping(value = "deleteunit", method = RequestMethod.GET)
-	public ModelAndView deleteUnitData(@RequestParam("id") int id) {
-		masterProductService.deleteUnit(id);
-		List<UnitBean> list=new ArrayList<UnitBean>();
-		list=masterProductService.getUnitsDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("unitlist", list);
-		return new ModelAndView("getunit",model);
-	}
-	
-	
-	
-	@RequestMapping(value = "getunit", method = RequestMethod.POST)
-	public void getUnit(@RequestParam("id") String id,HttpServletResponse res )throws ParseException  {
-		String curList=masterProductService.getUnitById(id);
-		System.out.println(curList);
-		try {
-			PrintWriter writter=res.getWriter();
-			writter.print(curList);
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-	
-	}
-	
-	
-	
-	
-	@RequestMapping(value="editunit",method=RequestMethod.POST)
-	@ResponseBody
-	public void editUnit(@RequestBody String json,Model model) throws IOException{
-	System.out.println("Deeoak              *****************************************************");
-		ObjectMapper mapper=new ObjectMapper();
-		UnitBean poref=mapper.readValue(json, UnitBean.class);
-		UnitBean poref1=new UnitBean();
-		poref1.setUnittypeid(poref.getUnittypeid());
-		poref1.setUnittype(poref.getUnittype());
-		
-		masterProductService.editUnit(poref1);
-		
-	}
-
-	
-	
-	@RequestMapping(value="/producthead",method = RequestMethod.GET)
-	public ModelAndView getProductHead(@ModelAttribute("producthead") ProductHeadBean productHeadBean,
-			HttpServletRequest request){
-		List<ProductHeadBean> list=new ArrayList<ProductHeadBean>();
-		list=masterProductService.getProductHeadDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("productlist", list);
-		return new ModelAndView("productheadpage",model);
-}
-
-	
-	@RequestMapping(value="addproduct",method=RequestMethod.POST)
-	@ResponseBody
-	public void addProducthead(@RequestBody String json,Model model) throws IOException{
-	System.out.println("Deeoak              ***************************************************** \n"+json);
-		ObjectMapper mapper=new ObjectMapper();
-		ProductHeadBean poref=mapper.readValue(json, ProductHeadBean.class);
-		ProductHeadBean poref1=new ProductHeadBean();
-		//poref1.setCurrencyid(poref.getCurrencyid());
-		poref1.setProductheadname(poref.getProductheadname());
-		
-		//poref.setPorefentryitemdetailid(null);
-		
-		
-		
-		masterProductService.addProductHead(poref1);
-		//model.addAttribute("prolist",  prepareListofBean(prinicipalposervice.proList()));
-	//return toJson1(poref1);
-	}
-	
-	
-	
-	@RequestMapping(value = "deleteproducthead", method = RequestMethod.GET)
-	public ModelAndView deleteProductHeadData(@RequestParam("id") int id) {
-		masterProductService.deleteProductHead(id);
-		List<ProductHeadBean> list=new ArrayList<ProductHeadBean>();
-		list=masterProductService.getProductHeadDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("productlist", list);
-		return new ModelAndView("productheadpage",model);
-	}
-	
-	
-	@RequestMapping(value="editproducthead",method=RequestMethod.POST)
-	@ResponseBody
-	public void editProducthead(@RequestBody String json,Model model) throws IOException{
-	System.out.println("Deeoak              *****************************************************");
-	ObjectMapper mapper=new ObjectMapper();
-	ProductHeadBean poref=mapper.readValue(json, ProductHeadBean.class);
-	ProductHeadBean poref1=new ProductHeadBean();
-	//poref1.setCurrencyid(poref.getCurrencyid());
-	poref1.setProductheadname(poref.getProductheadname());
-	poref1.setProductheadid(poref.getProductheadid());	
-		masterProductService.editProductHead(poref1);
-		
-	}
-	
-	
-	
-	@RequestMapping(value = "getproducthead", method = RequestMethod.POST)
-	public void getProductheadByID(@RequestParam("id") String id,HttpServletResponse res )throws ParseException  {
-		String curList=masterProductService.getProductHeadById(id);
-		System.out.println(curList);
-		try {
-			PrintWriter writter=res.getWriter();
-			writter.print(curList);
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-	}
-	
-	
-
-	@RequestMapping(value="/productsubhead",method = RequestMethod.GET)
-	public ModelAndView ProductSubHead(@ModelAttribute("producthead") ProductHeadBean productHeadBean,
-			HttpServletRequest request){
-		List<ProductHeadBean> list=new ArrayList<ProductHeadBean>();
-		list=masterProductService.getProductHeadDetails();
-		Map<String, Object> model= new HashMap<String,Object>();
-		model.put("productlist", list);
-		
-		
-		return new ModelAndView("productsubheadpage",model);
-}
-
-	
-	@RequestMapping(value="addproductSub",method=RequestMethod.POST)
-	@ResponseBody
-	public void addProductSubhead(@RequestBody String json,Model model) throws IOException{
-	System.out.println("Deeoak              ***************************************************** \n"+json);
-		ObjectMapper mapper=new ObjectMapper();
-		ProductSubHeadBean poref=mapper.readValue(json, ProductSubHeadBean.class);
-		ProductSubHeadBean poref1=new ProductSubHeadBean();
-		//poref1.setCurrencyid(poref.getCurrencyid());
-		poref1.setProductheadid(poref.getProductheadid());
-		poref1.setProductsubheadname(poref.getProductsubheadname());
-		
-		masterProductService.addProductSubHead(poref1);
-	}
 	
 	
 	
